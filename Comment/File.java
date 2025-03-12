a8724be708b5a905b5ca6a4cea4a222ca76c9fea
JesinthaJohnson <jesi272005@gmail.com>
2025-03-12 14:16:48 +0530

    multiline comment

--- Comment/File.java
+++ Comment/File.java
@@ -1,7 +1,11 @@
 public class File{
+    /*
+    class name file 
+    using numone as variablename
+    */
     public static void main(String args[])
     {
-        int numone = 10; //variable name
+        int numone = 10; 
         System.out.println(numone);
     }
 }
