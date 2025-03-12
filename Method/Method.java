a74a3b57558f1abc8004cc9a5848b8e179dea2ac
JesinthaJohnson <jesi272005@gmail.com>
2025-03-12 14:33:49 +0530

    method code

---
+++ Method/Method.java
@@ -0,0 +1,11 @@
+public class Method{
+    public void add(int numone,int numtwo){
+        int numthree =numone+numtwo;
+        System.out.println(numthree); 
+    }
+ public static void main(String args[]){
+
+        Method obj =new Method();
+        obj.add(10,2);
+        }
+    }
