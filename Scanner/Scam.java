3bb70372c9d948dc8b18f00460c854440bfcec49
JesinthaJohnson <jesi272005@gmail.com>
2025-03-13 15:11:00 +0530

    Scanner string code

---
+++ Scanner/Scam.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Scam{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the name");
+        String name=input.nextLine();
+        System.out.println(name);
+    }
+}
