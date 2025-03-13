558c253ef8fc093b114e9162b0aba45aaefe0018
JesinthaJohnson <jesi272005@gmail.com>
2025-03-13 10:35:08 +0530

    Conditional

---
+++ ConditionalOperator/Cond.java
@@ -0,0 +1,10 @@
+public class Cond{
+    public static void main(String args[]){
+        System.out.println(8<27);
+        System.out.println(10>2);
+        System.out.println(4<=27);
+        System.out.println(27>=4);
+        System.out.println(4==4);
+        System.out.println(27!=4);
+    }
+}
