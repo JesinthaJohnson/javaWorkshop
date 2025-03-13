9fa05c94ddb90ca978687421b406ce839334860b
JesinthaJohnson <jesi272005@gmail.com>
2025-03-13 11:09:01 +0530

    majorminor

---
+++ MajorMinorFinder/Major.java
@@ -0,0 +1,12 @@
+public class Major{
+    public static void main(String args[]){
+        int age=20;
+        if(age>=18){
+            System.out.println("major");
+        }
+        else{
+            System.out.println("Minor");
+        }
+        }
+
+    }
