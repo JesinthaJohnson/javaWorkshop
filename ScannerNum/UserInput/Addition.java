8b7bca9623f7413436594b529046deb53c6b0191
JesinthaJohnson <jesi272005@gmail.com>
2025-03-13 15:25:04 +0530

    Scanner Addition

---
+++ ScannerNum/UserInput/Addition.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Addition{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter number1");
+        int number1=s.nextInt();
+        System.out.println("Enter number2");
+        int number2=s.nextInt();
+        System.out.println(number1+number2);
+    }
+}
