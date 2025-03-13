c969840e4ebdad26bcc967bca86692febcb6ad3d
JesinthaJohnson <jesi272005@gmail.com>
2025-03-13 10:59:09 +0530

    If else code

---
+++ IfElse/Test.java
@@ -0,0 +1,10 @@
+public class Test{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("statement is true");
+        }
+ else{
+    System.out.println("The statement is false");
+
+ }
+}}
