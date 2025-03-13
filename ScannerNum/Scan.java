ad0206a1f85e5c753eaaee8d23ef98378685175c
JesinthaJohnson <jesi272005@gmail.com>
2025-03-13 15:18:15 +0530

    scanner age

---
+++ ScannerNum/Scan.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Scan{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the age");
+        int age=input.nextInt();
+        System.out.println(age);
+    }
+}
