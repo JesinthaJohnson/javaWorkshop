dcb0032038208900238b2467e845a72d8e63849b
JesinthaJohnson <jesi272005@gmail.com>
2025-03-13 10:49:27 +0530

    Conditional Statement

---
+++ ConditionalStatement/Condtrue.java
@@ -0,0 +1,12 @@
+public class Condtrue{
+    public static void main(String args[]){
+        if(true){
+
+System.out.println("Correct");
+        }
+   if(false) {
+    System.out.println("Wrong");
+
+   }
+}
+}
