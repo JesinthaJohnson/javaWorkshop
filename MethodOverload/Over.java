229ba2ff747590a1e13e7ce48e001fea4f623a28
JesinthaJohnson <jesi272005@gmail.com>
2025-03-14 12:17:16 +0530

    Method overload

---
+++ MethodOverload/Over.java
@@ -0,0 +1,16 @@
+public class Over
+{
+    void add(int a,int b)
+    {
+        System.out.println("answer is"+a+b);
+    }
+    void add (int a,int b,int c)
+    {
+        System.out.println("answer is"+a+b+c);
+    }
+    public static void main(String args[]){
+Over obj=new Over();
+obj.add(10,20);
+obj.add(10,20,30);
+    }
+}
