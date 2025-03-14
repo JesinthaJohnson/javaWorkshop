f15bdd73d4aa99eb392cd9819f0e0024021cd088
JesinthaJohnson <jesi272005@gmail.com>
2025-03-14 14:25:50 +0530

    Over Riding code

---
+++ OverRide/Over.java
@@ -0,0 +1,18 @@
+class Parent{
+    void display(){
+
+    }
+    }
+    class Child extends Parent{
+        void display(){
+            System.out.println("Hello");
+        }
+    }
+    public class Over{
+        public static void main(String args[]){
+            Child obj=new Child();
+            obj.display();
+        }
+    }
+        
+    
