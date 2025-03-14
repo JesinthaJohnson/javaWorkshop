1aae0a8f5fde0f89da47b946c710d785d5c33292
JesinthaJohnson <jesi272005@gmail.com>
2025-03-14 13:46:52 +0530

    MultilevelInheritance code

---
+++ MultiLevelInheritance/MUlti.java
@@ -0,0 +1,25 @@
+class Parent{
+    public void add(){
+        System.out.println("Hello World");
+    }
+}
+class Child extends Parent{
+    void display(){
+        add();
+
+    }
+}
+class Third extends Child{
+    void helper(){
+        display();
+            }
+}
+public class MUlti
+{
+    public static void main(String args[]){
+
+    
+    Third obj=new Third();
+    obj.helper();
+
+}}
