b119a6a426361bff7de31446e91b44af8dc2d627
JesinthaJohnson <jesi272005@gmail.com>
2025-03-14 12:01:37 +0530

    Caseconvert code

---
+++ CaseConvert/Convert.java
@@ -0,0 +1,9 @@
+public class Convert
+{
+    public static void main(String args[])
+    {
+        String name="JesiNTha";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
+}
